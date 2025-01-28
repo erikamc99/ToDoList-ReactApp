fd57238cd25754d397b4593569aa57fec5350b88
Erica <you@example.com>
2025-01-28 13:42:54 +0100

    functionality done, styles non finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
+import ToDoList from './components/ToDoList.jsx';
 
 function App() {
   
 
   return (
     <>
-      
+      <ToDoList />
     </>
   )
 }
